8b4636f99b09a746f200f8a0248a6983450c4ef2
kcw9609 <kang960953@gmail.com>
2025-05-23 17:30:39 +0900

    🔒️ Chore: CorsMapping 설정
    Chore: CorsMapping 설정

--- src/main/java/com/eyedia/eyedia/config/SecurityConfig.java
+++ src/main/java/com/eyedia/eyedia/config/SecurityConfig.java
@@ -4,9 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig implements WebMvcConfigurer {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -25,5 +27,19 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns(
+                        "http://localhost:8080",
+                        "http://54.180.228.18:8080",
+                        "http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowedHeaders("*")
+                .exposedHeaders("Set-Cookie")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
