5a7616d0d7a266409d27665c6c2558a957d91c4f
kcw9609 <kang960953@gmail.com>
2025-06-12 17:55:36 +0900

    👷 Chore: root api 추가
    Chore: root api 추가

---
+++ src/main/java/com/eyedia/eyedia/controller/RootController.java
@@ -0,0 +1,13 @@
+package com.eyedia.eyedia.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello, eyedia!";
+    }
+}
