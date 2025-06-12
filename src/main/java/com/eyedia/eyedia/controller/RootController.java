82ceb79c736bb10cf652f55074cfc0680a1a9a65
kcw9609 <kang960953@gmail.com>
2025-06-12 19:35:56 +0900

    👽️ Chore: api test 추가
    Chore: api test 추가

--- src/main/java/com/eyedia/eyedia/controller/RootController.java
+++ src/main/java/com/eyedia/eyedia/controller/RootController.java
@@ -1,6 +1,7 @@
 package com.eyedia.eyedia.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,5 +13,13 @@ public class RootController {
     public ResponseEntity<Void> checkHealthStatus() {
 
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+    @GetMapping("/health-check")
+    public ResponseEntity<String> checkHealth() {
+
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.TEXT_HTML)
+                .body("<h1>Welcome to Eyedia API</h1>");
     }
 }
