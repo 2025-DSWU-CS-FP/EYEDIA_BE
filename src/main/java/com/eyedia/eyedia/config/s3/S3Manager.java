9ebce52d50fb5b7dec6f37be6d841c7bfd1eec0e
20210699 <20210699@duksung.ac.kr>
2025-07-05 01:13:04 +0900

    ✨ Feat: s3 이미지 저장 경로 구현

--- src/main/java/com/eyedia/eyedia/config/s3/S3Manager.java
+++ src/main/java/com/eyedia/eyedia/config/s3/S3Manager.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @Slf4j @Component
 @RequiredArgsConstructor
@@ -28,5 +29,23 @@ public class S3Manager {
             log.error("S3 파일 업로드 실패 : {}", (Object) e.getStackTrace());
         }
         return amazonS3.getUrl(s3Config.getBucket(), keyName).toString();
+    }
+
+    // 객체 사진 저장은 다음 url을 통해 저장
+    public String generateObjectKeyName(String exhibitionTitle, String paintingTitle) {
+        final String uuid = UUID.randomUUID().toString();
+        return exhibitionTitle + '/' + paintingTitle + '/'  + uuid;
+    }
+
+    // 작품 사진 저장은 다음 url을 통해 저장
+    public String generatePaintingKeyName(String exhibitionTitle, String paintingTitle) {
+        final String uuid = UUID.randomUUID().toString();
+        return exhibitionTitle + '/' + paintingTitle + '/' + paintingTitle + '_' + uuid;
+    }
+
+    // 전시회 포스터 저장은 다음 url을 통해 저장
+    public String generateExhibitionKeyName(String exhibitionTitle) {
+        final String uuid = UUID.randomUUID().toString();
+        return exhibitionTitle + '/' + exhibitionTitle + '_' + uuid;
     }
 }
