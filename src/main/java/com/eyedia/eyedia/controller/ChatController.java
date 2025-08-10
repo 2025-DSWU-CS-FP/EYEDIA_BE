3df2bbb86c75eea6cc27e62414d0355ce775919f
kcw9609 <kang960953@gmail.com>
2025-08-10 15:24:35 +0900

    Chore: 채팅방 아이디로 구독

--- src/main/java/com/eyedia/eyedia/controller/ChatController.java
+++ src/main/java/com/eyedia/eyedia/controller/ChatController.java
@@ -30,13 +30,16 @@ public class ChatController {
     private static final String MODEL_API_URL = "http://localhost:8000/api/llm/answer"; // FastAPI 실제 API 주소
     private final PaintingRepository paintingRepository;
     private final MessageRepository messageRepository;
-
-    @MessageMapping("/echo")
+/**
+    //@MessageMapping("/echo/{roomId}")
+    //@SendTo("/room/{roomId}")
     public void echo(@Payload String message) {
         System.out.println("Echo received: " + message);
         // 구독 중인 모든 /room에 메시지 전송
         messagingTemplate.convertAndSend("/room", "echo: " + message);
     }
+*/
+
 
     @PostMapping("/paintings-push")
     public PaintingMetadataRequest pushPaintingDetected(@RequestBody PaintingMetadataRequest request,
