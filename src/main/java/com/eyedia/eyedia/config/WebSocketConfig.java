eb010a65c88d99dde27e49d77b0fe5b4ea3b79a0
kcw9609 <kang960953@gmail.com>
2025-08-10 14:54:35 +0900

    🔧 Config: 하트비트 설정 변경
    브로커에서 설정하도록 수정

--- src/main/java/com/eyedia/eyedia/config/WebSocketConfig.java
+++ src/main/java/com/eyedia/eyedia/config/WebSocketConfig.java
@@ -1,35 +1,29 @@
 package com.eyedia.eyedia.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.scheduling.TaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-
 @Configuration
 @EnableWebSocketMessageBroker
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private final AuthHandshakeInterceptor authHandshakeInterceptor;
-    private final TaskScheduler messageBrokerTaskScheduler; // ⬅ 주입
 
-    @Bean
-    public TaskScheduler messageBrokerTaskScheduler() {
-        var scheduler = new org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler();
-        scheduler.setPoolSize(1);
-        scheduler.setThreadNamePrefix("ws-heartbeat-");
-        scheduler.initialize();
-        return scheduler;
-    }
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
+        var ts = new org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler();
+        ts.setPoolSize(1);
+        ts.setThreadNamePrefix("ws-heartbeat-");
+        ts.initialize(); // ★ 반드시 초기화
+
         registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/topic","/queue", "/room")
-                .setHeartbeatValue(new long[]{10000,10000});
+        registry.enableSimpleBroker("/topic", "/queue", "/room")
+                .setTaskScheduler(ts)                         // ★ 바로 세팅
+                .setHeartbeatValue(new long[]{10000,10000});  // 하트비트 유지
     }
 
     @Override
