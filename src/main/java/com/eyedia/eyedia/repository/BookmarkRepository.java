2a13ce8b50da7c9bb3a537908c0b6324f9b424e9
kcw9609 <kang960953@gmail.com>
2025-08-28 11:47:29 +0900

    ✨ Feat: 사용자 방문 전시 필터링 및 검색 api 구현
    필터링 (전체 or 즐겨찾기) & (최신순 or 날짜순) & (검색어 있음 or 없음)

--- src/main/java/com/eyedia/eyedia/repository/BookmarkRepository.java
+++ src/main/java/com/eyedia/eyedia/repository/BookmarkRepository.java
@@ -1,9 +1,55 @@
 package com.eyedia.eyedia.repository;
 
+import com.eyedia.eyedia.domain.Exhibition;
 import com.eyedia.eyedia.domain.mapping.Bookmark;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BookmarkRepository  extends JpaRepository<Bookmark, Long> {
+
+    @Query("""
+    select e
+    from Bookmark b
+      join b.exhibition e
+    where b.user.usersId = :userId
+      and (:keyword is null or :keyword = ''
+           or lower(e.title) like lower(concat('%', :keyword, '%'))
+           or lower(e.gallery) like lower(concat('%', :keyword, '%')))
+    order by (
+        select max(v.visitedAt)
+        from Visit v
+        where v.user.usersId = :userId and v.exhibition = e
+    ) desc
+""")
+    Page<Exhibition> findBookmarkViewedOrderByRecent(
+            @Param("userId") Long userId,
+            @Param("keyword") String keyword,
+            Pageable pageable
+    );
+
+    @Query("""
+    select e
+    from Bookmark b
+      join b.exhibition e
+    where b.user.usersId = :userId
+      and (:keyword is null or :keyword = ''
+           or lower(e.title) like lower(concat('%', :keyword, '%'))
+           or lower(e.gallery) like lower(concat('%', :keyword, '%')))
+    order by (
+        select min(v.visitedAt)
+        from Visit v
+        where v.user.usersId = :userId and v.exhibition = e
+    ) asc
+""")
+    Page<Exhibition> findBookmarkViewedOrderByOldest(
+            @Param("userId") Long userId,
+            @Param("keyword") String keyword,
+            Pageable pageable
+    );
+
 
 
 }
