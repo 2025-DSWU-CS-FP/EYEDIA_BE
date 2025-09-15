deebb6dae27291bc296c705989785f291dca221a
20210699 <20210699@duksung.ac.kr>
2025-09-15 20:14:38 +0900

    ✏️ Fix: 엔드포인트 변경

--- src/main/java/com/eyedia/eyedia/controller/ChatController.java
+++ src/main/java/com/eyedia/eyedia/controller/ChatController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/api/v1/")
 public class ChatController {
 
     private final SimpMessagingTemplate messagingTemplate;
