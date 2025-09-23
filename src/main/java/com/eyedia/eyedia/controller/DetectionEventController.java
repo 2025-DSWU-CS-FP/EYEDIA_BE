815e8642f1736776f3b11365ba12a0f3bfc256d7
20210699 <20210699@duksung.ac.kr>
2025-09-23 16:41:23 +0900

    ✏️ Fix: 응답 API 수정

--- src/main/java/com/eyedia/eyedia/controller/DetectionEventController.java
+++ src/main/java/com/eyedia/eyedia/controller/DetectionEventController.java
@@ -1,22 +1,19 @@
 package com.eyedia.eyedia.controller;
 
 import com.eyedia.eyedia.domain.Painting;
-import com.eyedia.eyedia.dto.AreaDetectDTO;
 import com.eyedia.eyedia.dto.MessageDTO;
+import com.eyedia.eyedia.global.ApiResponse;
 import com.eyedia.eyedia.global.error.exception.GeneralException;
 import com.eyedia.eyedia.global.error.status.ErrorStatus;
+import com.eyedia.eyedia.global.error.status.SuccessStatus;
 import com.eyedia.eyedia.repository.ExhibitionRepository;
 import com.eyedia.eyedia.repository.PaintingRepository;
-import com.eyedia.eyedia.service.DocentChatService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/v1/events")
@@ -26,35 +23,35 @@ public class DetectionEventController {
     private final SimpMessageSendingOperations messagingTemplate;
     private final PaintingRepository paintingRepository;
     private final ExhibitionRepository exhibitionRepository;
-    private final DocentChatService docentChatService;
 
     @PostMapping("/detect")
-    public ResponseEntity<Void> detect(@RequestBody Long artId) {
+    public ApiResponse<?> detect(@RequestBody Long artId) {
+
+        MessageDTO.ChatImageResponseDTO message;
 
         var list = paintingRepository.findNullUserByArtId(artId);
         if (list.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+            throw new GeneralException(ErrorStatus.PAINTING_NOT_FOUND);
         } else if (list.size() > 1) {
-            throw new ResponseStatusException(HttpStatus.CONFLICT);
+            throw new GeneralException(ErrorStatus.PAINTING_CONFLICT);
         } else {
             Painting painting = list.get(0);
             var exhibition = exhibitionRepository.findByPaintingsPaintingId(painting.getPaintingId())
                     .orElseThrow(() -> new GeneralException(ErrorStatus.EXHIBITION_NOT_FOUND));
 
-            messagingTemplate.convertAndSend(
-                    "/queue/events",
-                    MessageDTO.ChatImageResponseDTO.builder()
-                            .paintingId(painting.getPaintingId())
-                            .imgUrl("https://s3-eyedia.s3.ap-northeast-2.amazonaws.com/"
-                                    + exhibition.getExhibitionsId() + "/" + artId + "/" + artId + ".jpg")
-                            .title(painting.getTitle())
-                            .artist(painting.getArtist())
-                            .description(painting.getDescription())
-                            .exhibition(exhibition.getTitle())
-                            .artId(artId)
-                            .build()
-            );
+            message = MessageDTO.ChatImageResponseDTO.builder()
+                    .paintingId(painting.getPaintingId())
+                    .imgUrl("https://s3-eyedia.s3.ap-northeast-2.amazonaws.com/"
+                            + exhibition.getExhibitionsId() + "/" + artId + "/" + artId + ".jpg")
+                    .title(painting.getTitle())
+                    .artist(painting.getArtist())
+                    .description(painting.getDescription())
+                    .exhibition(exhibition.getTitle())
+                    .artId(artId)
+                    .build();
+
+            messagingTemplate.convertAndSend("/queue/events", message);
         }
-        return ResponseEntity.ok().build();
+        return ApiResponse.of(SuccessStatus._OK, message);
     }
 }
