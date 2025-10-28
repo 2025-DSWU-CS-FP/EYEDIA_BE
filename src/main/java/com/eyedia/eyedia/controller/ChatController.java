40bd2dc20c26491b13707f4a011167054faa95e9
20210699 <20210699@duksung.ac.kr>
2025-10-28 16:47:57 +0900

    ♻️ Refactor : 추가 질문 웹소켓

--- src/main/java/com/eyedia/eyedia/controller/ChatController.java
+++ src/main/java/com/eyedia/eyedia/controller/ChatController.java
@@ -1,9 +1,18 @@
 package com.eyedia.eyedia.controller;
 
+import com.eyedia.eyedia.domain.Message;
+import com.eyedia.eyedia.domain.enums.SenderType;
 import com.eyedia.eyedia.dto.MessageDTO;
+import com.eyedia.eyedia.global.error.exception.GeneralException;
+import com.eyedia.eyedia.global.error.status.ErrorStatus;
+import com.eyedia.eyedia.repository.MessageRepository;
+import com.eyedia.eyedia.repository.PaintingRepository;
+import com.eyedia.eyedia.service.DocentChatService;
+import com.eyedia.eyedia.service.TtsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
@@ -14,10 +23,38 @@ import java.security.Principal;
 public class ChatController {
 
     private final SimpMessagingTemplate messagingTemplate;
+    private final DocentChatService docentChatService;
+    private final PaintingRepository paintingRepository;
+    private final MessageRepository messageRepository;
 
-    @MessageMapping("/chat.sendMessage")
-    public void sendMessage(@Payload MessageDTO.ChatMessageDTO m, Principal principal) {
-        String room = "/room/" + m.getPaintingId();
-        messagingTemplate.convertAndSendToUser(principal.getName(),room, m);
+    @MessageMapping("/ask")
+    public void sendMessage(@Payload MessageDTO.AskRequest req, Principal principal) {
+        var p = paintingRepository.findByPaintingId(req.getPaintingId())
+                .orElseThrow(() -> new GeneralException(ErrorStatus.PAINTING_NOT_FOUND));
+
+        Message q = Message.builder()
+                .sender(SenderType.USER)
+                .painting(p)
+                .content(req.getText())
+                .build();
+        messageRepository.save(q);
+
+        var answer = docentChatService.answer(docentChatService.basePrompt(p, req.getText()));
+
+        Message a = Message.builder()
+                .sender(SenderType.ASSISTANT)
+                .painting(p)
+                .content(answer.text())
+                .build();
+        messageRepository.save(a);
+
+        // 프론트로도 브로드캐스트
+        var dto = MessageDTO.ChatAnswerDTO.builder()
+                .paintingId(req.getPaintingId())
+                .answer(answer.text())
+                .model(answer.model())
+                .build();
+
+        messagingTemplate.convertAndSendToUser(principal.getName(),"/room/" + req.getPaintingId(), dto);
     }
 }
